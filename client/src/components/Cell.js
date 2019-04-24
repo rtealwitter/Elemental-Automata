fe4766595ffcd87ab2d8f5b639b006a64fb9e153
rtealw <r.teal.witter@gmail.com>
2019-04-24 14:43:17 -0400

    fix inconsistent click

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-//import Konva from 'konva';
-//import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 import { Rect } from 'react-konva';
 
 class Cell extends Component {
   constructor() {
     super();
+    this.changeElement = this.changeElement.bind(this);
+
     this.state = {};
   }
 
+  changeElement() {
+    const { handleChange, row, col } = this.props;
+    handleChange(row, col);
+  }
+
   render() {
+    const { x, y, dx, dy, color } = this.props;
     return (
       <Rect
-        x={this.props.x}
-        y={this.props.y}
-        height={this.props.dy}
-        width={this.props.dx}
-        fill={this.props.color}
+        x={x}
+        y={y}
+        height={dy}
+        width={dx}
+        fill={color}
+        onClick={this.changeElement.bind(this)}
       />
     );
   }
 }
+
+Cell.propTypes = {
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  dx: PropTypes.number.isRequired,
+  dy: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  row: PropTypes.number.isRequired,
+  col: PropTypes.number.isRequired
+};
 
 export default Cell;
