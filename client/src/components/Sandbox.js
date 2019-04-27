8367ca45add3095ffa7d2ebdfef8157dc2471a49
ABerry057 <awberry@middlebury.edu>
2019-04-27 16:56:20 -0400

    fixed linting errors

--- client/src/components/Sandbox.js
+++ client/src/components/Sandbox.js
@@ -85,11 +85,11 @@ class Sandbox extends Component {
         const today = new Date();
         let dd = today.getDate();
         let mm = today.getMonth() + 1;
-        let yyyy = today.getFullYear();
+        const yyyy = today.getFullYear();
 
-        if (dd < 10) dd = '0' + dd;
-        if (mm < 10) mm = '0' + mm;
-        return yyyy + '-' + mm + '-' + dd;
+        if (dd < 10) dd = `0${dd}`;
+        if (mm < 10) mm = `0${mm}`;
+        return `${yyyy}-${mm}-${dd}`;
       };
       const saveDate = getCurDate();
       const jsonGrid = JSON.stringify(this.state.grid);
