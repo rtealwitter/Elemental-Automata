54e2b7de45f22413e91ec0de6a976284b441ad3b
Seamus Turco <sturco@middlebury.edu>
2019-05-05 15:21:20 -0400

    moved change element into its own file

--- client/src/components/Sandbox.js
+++ client/src/components/Sandbox.js
@@ -4,6 +4,7 @@ import Cell from './Cell.js';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import logic from './Logic.js';
+import change from './change.js';
 
 const Div = styled.div`
   display: inline-block;
@@ -118,130 +119,10 @@ class Sandbox extends Component {
   }
 
   changeElement(row, col) {
-    if (mouseDown) {
-      const { grid } = this.state;
-      const newGrid = Array.from(grid);
-      if (this.props.size > 1) {
-        // will act the same for size 2 and size 3 to reduce redundencies
-        if (row > 0) {
-          // will do up always
-          if (col < this.state.dimension - 1) {
-            newGrid[row - 1][col + 1] = Object.assign(grid[row - 1][col + 1], {
-              // up and right
-              element: this.props.element
-            });
-            newGrid[row][col + 1] = Object.assign(grid[row][col + 1], {
-              // right
-              element: this.props.element
-            });
-          }
-          newGrid[row - 1][col] = Object.assign(grid[row - 1][col], {
-            // up
-            element: this.props.element
-          });
-        } else {
-          if (col < this.state.dimension - 1) {
-            // else just right
-            newGrid[row][col + 1] = Object.assign(grid[row][col + 1], {
-              // right
-              element: this.props.element
-            });
-          }
-        }
-      }
-      if (this.props.size > 2) {
-        // does not handle up and right to reduce redundencies
-        if (row < this.state.dimension - 1) {
-          // will do down always
-          if (col < this.state.dimension - 1) {
-            newGrid[row + 1][col + 1] = Object.assign(grid[row + 1][col + 1], {
-              // down and right
-              element: this.props.element
-            });
-          }
-          if (col > 0) {
-            // will do left and left down always
-            newGrid[row + 1][col - 1] = Object.assign(grid[row + 1][col - 1], {
-              // left down
-              element: this.props.element
-            });
-            newGrid[row][col - 1] = Object.assign(grid[row][col - 1], {
-              // left
-              element: this.props.element
-            });
-            if (row > 0) {
-              newGrid[row - 1][col - 1] = Object.assign(
-                grid[row - 1][col - 1],
-                {
-                  // left up
-                  element: this.props.element
-                }
-              );
-            }
-          }
-          newGrid[row + 1][col] = Object.assign(grid[row + 1][col], {
-            // down
-            element: this.props.element
-          });
-        }
-        if (row === this.state.dimension - 1 && col > 0) {
-          // does left and left up (opposite of dimension 2)
-          newGrid[row - 1][col - 1] = Object.assign(grid[row - 1][col - 1], {
-            element: this.props.element
-          });
-          newGrid[row][col - 1] = Object.assign(grid[row][col - 1], {
-            element: this.props.element
-          });
-        }
-      }
-      if (this.props.size > 3) {
-        if (row > 1) {
-          // does 2 above
-          newGrid[row - 2][col] = Object.assign(grid[row - 2][col], {
-            element: this.props.element
-          });
-          if (col < this.state.dimension - 1) {
-            newGrid[row - 2][col + 1] = Object.assign(grid[row - 2][col + 1], {
-              element: this.props.element
-            });
-          }
-          if (col < this.state.dimension - 2) {
-            // uppper right corner
-            newGrid[row - 2][col + 2] = Object.assign(grid[row - 2][col + 2], {
-              element: this.props.element
-            });
-          }
-          if (col > 0) {
-            // left corner
-            newGrid[row - 2][col - 1] = Object.assign(grid[row - 2][col - 1], {
-              element: this.props.element
-            });
-          }
-        }
-        if (row > 0 && col < this.state.dimension - 2) {
-          newGrid[row - 1][col + 2] = Object.assign(grid[row - 1][col + 2], {
-            element: this.props.element
-          });
-        }
-        if (col < this.state.dimension - 2) {
-          // right
-          newGrid[row][col + 2] = Object.assign(grid[row][col + 2], {
-            element: this.props.element
-          });
-        }
-        if (row < this.state.dimension - 1 && col < this.state.dimension - 2) {
-          // bottom right corner
-          newGrid[row + 1][col + 2] = Object.assign(grid[row + 1][col + 2], {
-            element: this.props.element
-          });
-        }
-      }
-      newGrid[row][col] = Object.assign(grid[row][col], {
-        // always do clicked cell
-        element: this.props.element
-      });
-      this.setState({ grid: newGrid });
-    }
+    const { dimension, grid } = this.state;
+    const { size, element } = this.props;
+    const newGrid = change(row, col, grid, size, dimension, element, mouseDown);
+    this.setState({ grid: newGrid });
   }
 
   randColor(element) {
